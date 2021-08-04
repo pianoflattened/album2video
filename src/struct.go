7170fd99969a1e34d2173bc161fa32af6be0ea15
quak <girafbot@gmail.com>
2021-08-04 01:36:04 -0400

    line 11

--- src/struct.go
+++ src/struct.go
@@ -8,4 +8,7 @@ type FormData struct {
     outputPath     string `json:"outputPath"`
 }
 
+// make sure u look up ffprobe test / example data so u can make the struct
+// unless u just wanna use gjson which is fine too considering how much effort that'll be
+
 type VideoData struct {}
